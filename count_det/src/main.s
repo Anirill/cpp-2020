25608c8fd42c1eba138826c6e1761aa4c3994bcc
Anirill <anirill@mail.ru>
2020-09-30 00:35:45 +0300

    Working

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,223 +1,67 @@
-.data
-.balign 4
-result:
-	.word 0
-.balign 4
-myvar_1:
-	.word 1
-.balign 4
-myvar_2:
-	.word 1
-.balign 4
-myvar_3:
-	.word 1
 .text
 .global count_det
 count_det:
-	mov r2, r0	
+	mov r3, #0  // result
 	
-	ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-	mul r3, r1, r3
-	ldr r1, addr_of_myvar_1
-        str r3, [r1]
-       
-	ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_1
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_1
-        str r3, [r1]
-
-
-
-        ldr r2, addr_of_result
-        ldr r2, [r2]
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
+	ldr r1, [r0]  
+	add r0, #16
+	ldr r2, [r0]
+	mul r1, r2, r1
+	add r0, #16
+	ldr r2, [r0]
+	mul r1, r2, r1
+	add r3, r1  // result = 0 + a11 * a22 * a33
 	
-	ldr r2, addr_of_result
-        ldr r2, [r2]
-	ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-	add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
+	sub r0, #28
+	ldr r1, [r0]
+        add r0, #16
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r0, #4
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r3, r1  // result + a12 * a23 * a31
 
-        ldr r2, addr_of_result
-        ldr r2, [r2]
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
+        sub r0, #16
+        ldr r1, [r0]
+        add r0, #4
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r0, #16
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r3, r1 // result + a13 * a21 * a32
 
-	
-	ldr r1, addr_of_myvar_1
-	mov r2, #-1
-	str r2, [r1]
-	
-	ldr r1, addr_of_myvar_2
-	mov r2, #-1
-        str r2, [r1]
+        sub r0, #28
+        ldr r1, [r0]
+        add r0, #20
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r0, #8
+        ldr r2, [r0]
+        mul r1, r2, r1
+        sub r3, r1  // result -  a11 * a23 * a32
 
-	ldr r1, addr_of_myvar_3
-	mov r2, #-1
-        str r2, [r1]
+        sub r0, #24
+        ldr r1, [r0]
+        add r0, #8
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r0, #20
+        ldr r2, [r0]
+        mul r1, r2, r1
+        sub r3, r1  // result -  a12 * a21 * a33
 
-	mov r2, r0
+        sub r0, #24
+        ldr r1, [r0]
+        add r0, #8
+        ldr r2, [r0]
+        mul r1, r2, r1
+        add r0, #8
+        ldr r2, [r0]
+        mul r1, r2, r1
+        sub r3, r1  // result -  a13 * a22 * a31
 
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_1
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_1
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_3
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_1
-        str r3, [r1]
-
-        ldrb r3, [r2], #4
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        mul r3, r1, r3
-        ldr r1, addr_of_myvar_2
-        str r3, [r1]
-
-
-        ldr r2, addr_of_result
-        ldr r2, [r2]
-        ldr r1, addr_of_myvar_1
-        ldr r1, [r1]
-        add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
-
-        ldr r2, addr_of_result
-        ldr r2, [r2]
-        ldr r1, addr_of_myvar_2
-        ldr r1, [r1]
-        add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
-
-        ldr r2, addr_of_result
-        ldr r2, [r2]
-        ldr r1, addr_of_myvar_3
-        ldr r1, [r1]
-        add r2, r1, r2
-        ldr r1, addr_of_result
-        str r2, [r1]
-
-
-
-	ldr r0, [r1]
+	mov r0, r3  
 	bx lr
-
-
-addr_of_result : .word result
-addr_of_myvar_1 : .word myvar_1
-addr_of_myvar_2 : .word myvar_2
-addr_of_myvar_3 : .word myvar_3
-
-	
 
