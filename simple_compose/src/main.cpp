99b027bb3313ad00eb7c172bbfde5eb715a25187
Anirill <anirill@mail.ru>
2020-10-17 20:58:31 +0300

    re-formatted to GCS

--- simple_compose/src/main.cpp
+++ simple_compose/src/main.cpp
@@ -1,86 +1,75 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP,
+Ruby, C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS Code,
+Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include <iostream>
 #include <functional>
+#include <iostream>
 
-typedef std::function<int (int)> Op;
+typedef std::function<int(int)> Op;
 
-Op compose (size_t n, Op ops[]) {
-    Op idm = [] (int x) -> int {  // idempotent function
-            return x;
-        };
+Op compose(size_t n, Op ops[]) {
+  Op idm = [](int x) -> int {  // idempotent function
+    return x;
+  };
 
-    if (n == 0) return idm;
-    if (n == 1) return ops[0];
+  if (n == 0) return idm;
+  if (n == 1) return ops[0];
 
-    auto mul = [] (Op f, Op g) {  // simple composition
-        return [f, g] (auto x) {
-            return f(g(x));
-            };
-        };
+  auto mul = [](Op f, Op g) {  // simple composition
+    return [f, g](auto x) { return f(g(x)); };
+  };
 
-    return mul(ops[0], compose(n - 1, ops + 1));
+  return mul(ops[0], compose(n - 1, ops + 1));
 }
 
+int main() {
+  /// Simple tests:
 
-int main () {
-    /// Simple tests:
+  Op op1 = [](int x) -> int { return x + 1; };
 
-    Op op1 =
-        [] (int x) -> int {
-            return x + 1;
-        };
+  auto op2 = [](int p) -> Op { return [p](int x) { return p * x; }; };
 
-    auto op2 =
-        [] (int p) -> Op {
-            return [p] (int x) {
-                return p * x;
-            };
-        };
-
-    {
-        Op ops[4] = {op1, op2(2), op1, op2(2)};
-        if (compose(4, ops)(2) != 11) {
-            std::cout << "FAILED AT TEST 1" << std::endl;
-            return 0;
-        }
+  {
+    Op ops[4] = {op1, op2(2), op1, op2(2)};
+    if (compose(4, ops)(2) != 11) {
+      std::cout << "FAILED AT TEST 1" << std::endl;
+      return 0;
     }
+  }
 
-    {
-        Op ops[8] = {op1, op2(2), op1, op1, op1, op2(2), op2(2), op1};
-        if (compose(8, ops)(5) != 55) {
-            std::cout << "FAILED AT TEST 2" << std::endl;
-            return 0;
-        }
+  {
+    Op ops[8] = {op1, op2(2), op1, op1, op1, op2(2), op2(2), op1};
+    if (compose(8, ops)(5) != 55) {
+      std::cout << "FAILED AT TEST 2" << std::endl;
+      return 0;
     }
+  }
 
-    {
-        Op ops[1] = {op1};
-        if (compose(1, ops)(3) != 4) {
-            std::cout << "FAILED AT TEST 3" << std::endl;
-            return 0;
-        }
+  {
+    Op ops[1] = {op1};
+    if (compose(1, ops)(3) != 4) {
+      std::cout << "FAILED AT TEST 3" << std::endl;
+      return 0;
     }
+  }
 
-    {
-        Op ops[0] = {};
-        if (compose(0, ops)(4) != 4) {
-            std::cout << "FAILED AT TEST 4" << std::endl;
-            return 0;
-        }
+  {
+    Op ops[0] = {};
+    if (compose(0, ops)(4) != 4) {
+      std::cout << "FAILED AT TEST 4" << std::endl;
+      return 0;
     }
+  }
 
-    {
-        Op ops[4] = {op2(2), op2(3), op2(4), op2(5)};
-        if (compose(4, ops)(1) != 120) {
-            std::cout << "FAILED AT TEST 5" << std::endl;
-            return 0;
-        }
+  {
+    Op ops[4] = {op2(2), op2(3), op2(4), op2(5)};
+    if (compose(4, ops)(1) != 120) {
+      std::cout << "FAILED AT TEST 5" << std::endl;
+      return 0;
     }
+  }
 }
