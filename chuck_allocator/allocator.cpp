75fd9f04c8c7e1334ac22c2cd12976f9015fa6ad
Anirill <anirill@mail.ru>
2020-10-26 00:44:04 +0300

    Ready, GCS

--- chuck_allocator/allocator.cpp
+++ chuck_allocator/allocator.cpp
@@ -1,103 +1,115 @@
 #include "allocator.h"
-#include <vector>
-//#include <memory>
+
 #include <iostream>
+#include <vector>
 
 using namespace task;
-class A {    
-public:
-    int x;
-    int y;
-    A (int x, int y) { this->x = x; this->y = y;} 
-    ~A () {;}
+class A {
+ public:
+  int x;
+  int y;
+  A(int x, int y) {
+    this->x = x;
+    this->y = y;
+  }
+  ~A() { ; }
 };
 
-bool check_int_array () {  
-    Allocator<int> allint;
+bool check_int_array() {
+  Allocator<int> allint;
 
-    int *arr;
-    int *arr2;
-    int *arr3;
+  int *arr;
+  int *arr2;
+  int *arr3;
 
-    arr = allint.allocate(500);  // chunk is 2048b i.e. 512 * sizeof(int)
-    arr2 = allint.allocate(500);
-    arr3 = allint.allocate(10);
+  arr = allint.allocate(500);  // chunk is 2048b i.e. 512 * sizeof(int)
+  arr2 = allint.allocate(500);
+  arr3 = allint.allocate(10);
 
-    if (arr3 - arr > 0x0800 || arr3 - arr < 0) return false;  // arr3 goes to 1st chunk
-    for (int i = 0; i < 10; i++) arr3[i] = 1000 * i; // filling arrays
-    for (int i = 0; i < 500; i++) {
-        arr[i] = i;
-        arr2[i] = i + 1000;
-    }
+  if (arr3 - arr > 0x0800 || arr3 - arr < 0)
+    return false;                                   // arr3 goes to 1st chunk
+  for (int i = 0; i < 10; i++) arr3[i] = 1000 * i;  // filling arrays
+  for (int i = 0; i < 500; i++) {
+    arr[i] = i;
+    arr2[i] = i + 1000;
+  }
 
-    for (int i = 0; i < 10; i++) {  
-        if(arr3[i] != 1000 * i) return false;  // checking for valid data
-    }
-    for (int i = 0; i < 500; i++) {
-        if(arr[i] != i) return false;
-        if(arr[i] != i + 1000) return false;
-    }
-    return true;
+  for (int i = 0; i < 10; i++) {
+    if (arr3[i] != 1000 * i) return false;  // checking for valid data
+  }
+  for (int i = 0; i < 500; i++) {
+    if (arr[i] != i) return false;
+    if (arr[i] != i + 1000) return false;
+  }
+  return true;
 }
 
+bool check_vector() {
+  std::vector<A, Allocator<A>> vec;
 
-bool check_vector () {
-    std::vector<A, Allocator<A>> vec;
+  for (int i = 0; i < 256; i++)
+    vec.push_back(
+        A(i, 1000 + i));  // vector reallocates itself requiring 2nd chunk
 
-    for (int i = 0; i < 256; i++) vec.push_back(A(i, 1000 + i)); // vector reallocates itself requiring 2nd chunk
-
-    for (int i = 0; i < 256; i++) {
-        if(vec[i].x != i) return false;         // checking for valid data
-        if(vec[i].y != 1000 + i) return false;  // checking for valid data        
-    }
-    return true;
+  for (int i = 0; i < 256; i++) {
+    if (vec[i].x != i) return false;         // checking for valid data
+    if (vec[i].y != 1000 + i) return false;  // checking for valid data
+  }
+  return true;
 }
 
+bool check_copy() {
+  Allocator<A> alloc;
+  A *a[20];
 
-bool check_copy () {
-    Allocator<A> alloc;
-    A* a[20];
+  a[0] = alloc.allocate(1);  // allocating 1st chunk
 
-    a[0] = alloc.allocate(1);     // allocating 1st chunk
+  Allocator<A> alloc2 = alloc;  // copy alloc
 
-    Allocator<A> alloc2 = alloc;  // copy alloc
+  for (int i = 1; i < 20; i++) {
+    if (i % 2)
+      a[i] = alloc.allocate(1);  // allocating with alloc
+    else
+      a[i] = alloc2.allocate(1);         // allocating with alloc2
+    alloc.construct(a[i], i, 1000 + i);  // filling array
+  }
 
-    for (int i = 1; i < 20; i++) 
-    {
-        if(i % 2) a[i] = alloc.allocate(1);  // allocating with alloc
-        else a[i] = alloc2.allocate(1);      // allocating with alloc2
-        alloc.construct(a[i], i, 1000 + i);  // filling array
-    }
+  for (int i = 0; i < 20; i++) {
+    if (a[i]->x != i) return false;         // checking for valid data
+    if (a[i]->y != 1000 + i) return false;  // checking for valid data
+  }
 
-    for (int i = 0; i < 20; i++) {
-        if(a[i]->x != i) return false;         // checking for valid data
-        if(a[i]->y != 1000 + i) return false;  // checking for valid data      
-    }
+  if (a[1] - a[0] != 8) return false;
 
-    if(a[1] - a[0] != 8) return false;
+  alloc2.~Allocator();  // destruct alloc2
 
-    alloc2.~Allocator();   // destruct alloc2
+  for (int i = 0; i < 20; i++) {
+    if (a[i]->x != i)
+      return false;  // checking for valid data after destructing one of the
+                     // allocators
+    if (a[i]->y != 1000 + i)
+      return false;  // checking for valid data after destructing one of the
+                     // allocators
+  }
 
-    for (int i = 0; i < 20; i++) {
-        if(a[i]->x != i) return false;         // checking for valid data after destructing one of the allocators
-        if(a[i]->y != 1000 + i) return false;  // checking for valid data after destructing one of the allocators      
-    }
-
-    return true;
+  return true;
 }
 
+int main() {
+  if (check_int_array)
+    std::cout << "Int array check valid!\n";
+  else
+    std::cout << "Int array check failed!\n";
 
-int main()
-{   
-    if(check_int_array) std::cout << "Int array check valid!\n";
-    else std::cout << "Int array check failed!\n";    
-    
-    if(check_vector) std::cout << "Vector check valid!\n";
-    else std::cout << "Vector check failed!\n";    
-    
-    if(check_copy) std::cout << "Copy check valid!\n";
-    else std::cout << "Copy check failed!\n";   
-    
+  if (check_vector)
+    std::cout << "Vector check valid!\n";
+  else
+    std::cout << "Vector check failed!\n";
 
-    return 0;
+  if (check_copy)
+    std::cout << "Copy check valid!\n";
+  else
+    std::cout << "Copy check failed!\n";
+
+  return 0;
 }
--- chuck_allocator/allocator.h
+++ chuck_allocator/allocator.h
@@ -1,124 +1,115 @@
 #pragma once
 #include <iostream>
-//#include <memory>
-//#include <vector>
 
 const size_t BLOCK_SIZE = 2048;
 
 namespace task {
 
-
 struct Chunk {
-    char *start;
-    size_t remainder;
-    char *current;
-    Chunk *next;
+  char *start;
+  size_t remainder;
+  char *current;
+  Chunk *next;
 
-    Chunk() {
-        std::cout << "Chunk\n";
-        this->start = new char[BLOCK_SIZE]; 
-        this->current = this->start;
-        this->remainder = BLOCK_SIZE;
-        this->next = NULL;
+  Chunk() {
+    std::cout << "Chunk\n";
+    this->start = new char[BLOCK_SIZE];
+    this->current = this->start;
+    this->remainder = BLOCK_SIZE;
+    this->next = NULL;
+  }
+
+  ~Chunk() {
+    if (this->next != NULL) this->next->~Chunk();
+    if (this->start != NULL) delete[] this->start;
+    if (this->next != NULL) {
+      delete this->next;
+      this->next = NULL;
     }
-    
-    ~Chunk() {        
-        if (this->next != NULL) this->next->~Chunk();
-        if (this->start != NULL) delete[] this->start;
-        if (this->next != NULL) {
-            delete this->next; 
-            this->next = NULL;
-            //std::cout << "Deallocating chunkC!\n"; 
-        }  
-        //std::cout << "Deallocating chunkC!\n";       
+  }
+
+  char *memory_request(const size_t size) {
+    if (size > BLOCK_SIZE) return NULL;
+    if (size <= this->remainder) {
+      // std::cout << "size ok! \n";
+      this->current += size;
+      this->remainder -= size;
+      return (this->current - size);
+    } else if (this->next != NULL) {
+      // std::cout << "Requesting next! \n";
+      return this->next->memory_request(size);
+    } else {
+      // std::cout << "Creating new! \n";
+      this->next = new Chunk();
+      return this->next->memory_request(size);
     }
-
-    char* memory_request(const size_t size) {
-        if (size > BLOCK_SIZE) return NULL;
-        if (size <= this->remainder) {
-            std::cout << "size ok! \n";
-            this->current += size;
-            this->remainder -= size;
-            return (this->current - size);
-        }
-        else if (this->next != NULL) {
-            std::cout << "Requesting next! \n";
-            return this->next->memory_request(size);
-        }
-        else {
-            std::cout << "Creating new! \n";
-            this->next = new Chunk();
-            return this->next->memory_request(size);
-        }
-    }
-};  
-
-template<typename T>
-class Allocator {
-    size_t *chunk_counter;
-    Chunk *chunks;
-public:
-    using size_type = std::size_t;
-    using pointer = T*;
-    using value_type = T;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
-    using difference_type = std::ptrdiff_t;
-    template<class U> 
-    struct rebind { typedef Allocator<U> other; };
-
-    Allocator () { chunks = NULL; chunk_counter = new size_t(0); }
-
-    Allocator (const Allocator &a) : chunks(a.chunks), chunk_counter(a.chunk_counter) {
-        *this->chunk_counter += 1;
-        std::cout << "CC: " << *this->chunk_counter << "\n";
-    }
-
-    ~Allocator () {            
-        std::cout << "D CC: " << *this->chunk_counter << "\n";
-        if(*this->chunk_counter == 1) {
-            if (chunks != NULL) {
-                std::cout << "Deallocating chunk!\n";
-                chunks->~Chunk();
-                delete chunks;                
-            }            
-        }
-        else if (*this->chunk_counter > 0) *this->chunk_counter -= 1;
-    }
-
-    template <typename ... Args>
-    void construct (T* p, Args&&... args) {
-        new (p) T(args...);
-        return;
-    };
-
-    void destroy (T* p) {
-        p->~T();
-    };
-
-    T* allocate (const size_t size) {
-        //std::cout << "Allocate: " << chunks << "\n";
-        if (chunks == NULL) { 
-            chunks = new Chunk; 
-            *chunk_counter = 1;
-            std::cout << "CC: " << *chunk_counter << "\n";
-        }        
-        return (T*) chunks->memory_request(size * sizeof(T));
-    };
-
-    void deallocate (T*, const size_t n) {;}
-
-    Allocator& operator= (Allocator const &a) {
-        if(this != &a) {
-            Allocator tmp(a);
-            this = &tmp;
-        }
-        return *this;
-    };
-
-    size_t max_size () {
-        return BLOCK_SIZE;
-    }    
+  }
 };
-}
+
+template <typename T>
+class Allocator {
+  size_t *chunk_counter;
+  Chunk *chunks;  // pointer to 1st chunk
+ public:
+  using size_type = std::size_t;
+  using pointer = T *;
+  using value_type = T;
+  using const_pointer = const T *;
+  using reference = T &;
+  using const_reference = const T &;
+  using difference_type = std::ptrdiff_t;
+  template <class U>
+  struct rebind {
+    typedef Allocator<U> other;
+  };
+
+  Allocator() {
+    chunks = NULL;
+    chunk_counter = new size_t(0);
+  }
+
+  Allocator(const Allocator &a)
+      : chunks(a.chunks), chunk_counter(a.chunk_counter) {
+    *this->chunk_counter += 1;  // remeber copy
+  }
+
+  ~Allocator() {
+    if (*this->chunk_counter == 1) {  // check for active copy
+      if (chunks != NULL) {
+        chunks->~Chunk();
+        delete chunks;
+      }
+    } else if (*this->chunk_counter > 0)
+      *this->chunk_counter -=
+          1;  // if no chunk was created, no need to decrease chunk_counter
+  }
+
+  template <typename... Args>
+  void construct(T *p, Args &&... args) {
+    new (p) T(args...);
+    return;
+  };
+
+  void destroy(T *p) { p->~T(); };
+
+  T *allocate(const size_t size) {
+    if (chunks == NULL) {
+      chunks = new Chunk;  // allocate on demand
+      *chunk_counter = 1;  // remember 1st chunk
+    }
+    return (T *)chunks->memory_request(size * sizeof(T));
+  };
+
+  void deallocate(T *, const size_t n) { ; }
+
+  Allocator &operator=(Allocator const &a) {
+    if (this != &a) {
+      Allocator tmp(a);  // copy constructor
+      this = &tmp;
+    }
+    return *this;
+  };
+
+  size_t max_size() { return BLOCK_SIZE; }
+};
+}  // namespace task
