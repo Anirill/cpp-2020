e99f6ddd7465e78c3e935ef864ada60c207282c7
Anirill <62198933+Anirill@users.noreply.github.com>
2020-11-21 02:21:38 +0300

    Update game.h
    Last one

--- hw_7_meanwhile_in_the_seventh_galaxy/first_variant/src/game.h
+++ hw_7_meanwhile_in_the_seventh_galaxy/first_variant/src/game.h
@@ -4,45 +4,75 @@
 #include "xor_sum.h"
 #include "binary.h"
 
-//class NullType {};
+////////////////   Declaration
 
-template <size_t Head, size_t... Tail>
+template <
+    size_t xs, 
+    size_t Head, 
+    size_t... Tail
+    >
 struct FindHeap;
 
-////////////////   Conditional MAX search
+template <size_t xs, size_t... Heaps>
+struct StartSearch;
 
-template <bool cond, size_t Head, size_t... Tail>
+////////////////   Conditional HEAP search
+
+template <
+    bool cond, 
+    size_t xs, 
+    size_t Head, 
+    size_t... Tail
+    >
 struct  Search {
-    static constexpr ssize_t num = FindHeap<Tail...>::num;
-    static constexpr ssize_t value = FindHeap<Tail...>::value;
-    static constexpr ssize_t heap_to_use = FindHeap<Tail...>::heap_to_use;
+    static constexpr ssize_t num = FindHeap<xs, Tail...>::num;
+    static constexpr ssize_t value = FindHeap<xs, Tail...>::value;
+    static constexpr ssize_t heap_to_use = FindHeap<xs, Tail...>::heap_to_use;
 };
 
-template <size_t Head, size_t... Tail>
-struct Search<true, Head, Tail...> {
-    static constexpr ssize_t num = FindHeap<Tail...>::num;
+template <
+    size_t xs, 
+    size_t Head, 
+    size_t... Tail
+    >
+struct Search<true, xs, Head, Tail...> {
+    static constexpr ssize_t num = FindHeap<xs, Tail...>::num;
     static constexpr ssize_t value = Head;
     static constexpr ssize_t heap_to_use = num;
 };
 
-template <bool cond, size_t Head>
-struct  Search<cond, Head> {
+template <bool cond, size_t xs, size_t Head>
+struct  Search<cond, xs, Head> {
     static constexpr ssize_t num = 0;
     static constexpr ssize_t value = Head;
     static constexpr ssize_t heap_to_use = 0;
 };
 
-////////////////   Unconditional MAX search
+////////////////   Unconditional HEAP search
 
-template <size_t Head, size_t... Tail> // first and mid spec
+template <size_t xs, size_t Head, size_t... Tail> // first and mid spec
 struct FindHeap {
-    static constexpr ssize_t num = 1 + FindHeap<Tail...>::num;  // count heaps here
-    static constexpr ssize_t value = Search<(GetBit<Log2<XorSum<Head, Tail...>::value>::value, Head>::value == 1), Head, Tail...>::value; 
-    static constexpr ssize_t heap_to_use = Search<(GetBit<Log2<XorSum<Head, Tail...>::value>::value, Head>::value == 1), Head, Tail...>::heap_to_use;   
+    static constexpr ssize_t num = 1 + FindHeap<xs, Tail...>::num;  // count heaps here
+
+    static constexpr ssize_t value = 
+        Search<
+            ((GetBit<Log2<xs>::value - 1, Head>::value == 1)),     
+            xs,        
+            Head, 
+            Tail...
+        >::value; 
+
+    static constexpr ssize_t heap_to_use = 
+        Search<
+            ((GetBit<Log2<xs>::value - 1, Head>::value == 1)), 
+            xs,            
+            Head, 
+            Tail...
+        >::heap_to_use;   
 };
 
-template <size_t Head>  // last spec
-struct  FindHeap<Head> {
+template <size_t xs, size_t Head>  // last spec
+struct  FindHeap<xs, Head> {
     static constexpr ssize_t num = 1;  // last element is count 1
     static constexpr ssize_t value = Head;
     static constexpr ssize_t heap_to_use = 0;  // from last element
@@ -50,9 +80,9 @@ struct  FindHeap<Head> {
 
 ////////////////   Heap index calculate
 
-template <size_t x, size_t... Heaps>  // usual spec
+template <size_t xs, size_t... Heaps>  // usual spec
 struct HeapIndex {
-    static constexpr ssize_t value = FindHeap<Heaps...>::num - FindHeap<Heaps...>::heap_to_use - 1;
+    static constexpr ssize_t value = StartSearch<xs, Heaps...>::num - StartSearch<xs, Heaps...>::heap_to_use - 1;
 };
 
 template <size_t... Heaps>  // ZERO XOR spec
@@ -62,26 +92,42 @@ struct HeapIndex<0, Heaps...> {
 
 ////////////////   Rocks count calculate
 
-template<size_t xor_res, size_t max_res, size_t... Heaps>  // usual spec
+template <size_t xor_res, size_t search_res, size_t... Heaps>  // usual spec
 struct RocksCount { 
-    static constexpr ssize_t value = max_res - XorSum<XorSum<Heaps...>::value, max_res>::value;
+    static constexpr ssize_t value = search_res - XorSum<XorSum<Heaps...>::value, search_res>::value;
 };
 
-template<size_t max_res, size_t... Heaps>  // ZERO XOR spec
-struct RocksCount<0, max_res, Heaps...> {
+template <size_t search_res, size_t... Heaps>  // ZERO XOR spec
+struct RocksCount<0, search_res, Heaps...> {
     static constexpr ssize_t value = -1;
 };
 
 ////////////////   Main part
 
+template <size_t xs, size_t... Heaps>  // usual spec
+struct StartSearch {
+    static constexpr ssize_t num = FindHeap<xs, Heaps...>::num;
+    static constexpr ssize_t value = FindHeap<xs, Heaps...>::value;
+    static constexpr ssize_t heap_to_use = FindHeap<xs, Heaps...>::heap_to_use;
+};
+
+template <size_t... Heaps>  // ZERO XOR spec
+struct StartSearch<0, Heaps...> {
+    static constexpr ssize_t num = 0;
+    static constexpr ssize_t value = 0;
+    static constexpr ssize_t heap_to_use = 0;
+
+};
+
 template <size_t... Heaps>
 struct Game {
     static constexpr bool first_player_wins = XorSum<Heaps...>::value;
     static constexpr ssize_t first_move_heap_index = HeapIndex<XorSum<Heaps...>::value, Heaps...>::value;
-    static constexpr ssize_t first_move_rocks_count = RocksCount<XorSum<Heaps...>::value, FindHeap<Heaps...>::value, Heaps...>::value;
-    static constexpr ssize_t maxx = FindHeap<Heaps...>::value;
-    static constexpr ssize_t maxx_num = FindHeap<Heaps...>::num - FindHeap<Heaps...>::heap_to_use;
-    
+    static constexpr ssize_t first_move_rocks_count = RocksCount<
+        XorSum<Heaps...>::value, 
+        StartSearch<XorSum<Heaps...>::value, Heaps...>::value, 
+        Heaps...
+        >::value;   
 };
 
 
